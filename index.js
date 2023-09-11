38e26d86012f90a64048a235eb7bd99c2d28aa1d
EfrainRodriguez <efrainrg92@gmail.com>
2023-09-11 08:58:54 -0500

    uploading project :tada:

---
+++ index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const readData = () => {
+  try {
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const writeData = (data) => {
+  try {
+    fs.writeFileSync("./db.json", JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my first API with Node js!");
+});
+
+app.get("/books", (req, res) => {
+  const data = readData();
+  res.json(data.books);
+});
+
+app.get("/books/:id", (req, res) => {
+  const data = readData();
+  const id = parseInt(req.params.id);
+  const book = data.books.find((book) => book.id === id);
+  res.json(book);
+});
+
+app.post("/books", (req, res) => {
+  const data = readData();
+  const body = req.body;
+  const newBook = {
+    id: data.books.length + 1,
+    ...body,
+  };
+  data.books.push(newBook);
+  writeData(data);
+  res.json(newBook);
+});
+
+app.put("/books/:id", (req, res) => {
+  const data = readData();
+  const body = req.body;
+  const id = parseInt(req.params.id);
+  const bookIndex = data.books.findIndex((book) => book.id === id);
+  data.books[bookIndex] = {
+    ...data.books[bookIndex],
+    ...body,
+  };
+  writeData(data);
+  res.json({ message: "Book updated successfully" });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const data = readData();
+  const id = parseInt(req.params.id);
+  const bookIndex = data.books.findIndex((book) => book.id === id);
+  data.books.splice(bookIndex, 1);
+  writeData(data);
+  res.json({ message: "Book deleted successfully" });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
---
+++ requests.http
@@ -0,0 +1,25 @@
+GET http://localhost:3000/books HTTP/1.1
+
+###
+GET http://localhost:3000/books/1 HTTP/1.1
+
+###
+POST http://localhost:3000/books HTTP/1.1
+Content-Type: application/json
+
+{
+  "title": "Una breve historia del tiempo",
+  "author": "Stephen Hawking",
+  "year": 1988
+}
+
+###
+PUT http://localhost:3000/books/3 HTTP/1.1
+Content-Type: application/json
+
+{
+  "year": 1989
+}
+
+###
+DELETE http://localhost:3000/books/3 HTTP/1.1
